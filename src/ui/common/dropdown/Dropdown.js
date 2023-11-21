cfcdbebb325ecc1552b98b27ec0c9bfba03ed22c
olenababii <olenababii@MacBook-Pro-olenababii.local>
2023-11-21 23:20:23 +0200

    different type of q/a

--- src/ui/common/dropdown/Dropdown.js
+++ src/ui/common/dropdown/Dropdown.js
@@ -1,13 +1,32 @@
-export const Dropdown = ({ answers, onChange }) => {
+export const Dropdown = ({ answers, onChange, name }) => {
   return (
-    <select
-      className="border w-full p-2 rounded-xl m-2"
-      onChange={(answer) => onChange(answer)}
-    >
-      <option value="default" disabled selected></option>
-      {answers?.map((value) => (
-        <option value={value}>{value}</option>
-      ))}
-    </select>
+    <>
+      {answers.length > 2 ? (
+        <select
+          className="border w-full p-2 rounded-xl m-2"
+          onChange={(answer) => onChange(answer)}
+        >
+          <option value="default" disabled selected></option>
+          {answers?.map((value) => (
+            <option value={value}>{value}</option>
+          ))}
+        </select>
+      ) : (
+        <div className="flex flex-col m-2">
+          {answers?.map((value) => (
+            <label key={value} className="inline-flex items-center">
+              <input
+                type="radio"
+                name={name}
+                value={value}
+                onChange={(value) => onChange(value)}
+                className="form-radio h-5 w-5 text-blue-600"
+              />
+              <span className="ml-2">{value}</span>
+            </label>
+          ))}
+        </div>
+      )}{" "}
+    </>
   );
 };
--- src/ui/common/form/question/Question.js
+++ src/ui/common/form/question/Question.js
@@ -4,7 +4,7 @@ export const Question = ({ question, answers, onChange }) => {
   return (
     <div className="flex flex-col md:items-center justify-between w-full py-6">
       <p className="text-2xl">{question}</p>
-      <Dropdown answers={answers} onChange={onChange}/>
+      <Dropdown answers={answers} onChange={onChange} name={question}/>
     </div>
   );
 };
