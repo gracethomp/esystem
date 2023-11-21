fb9d71c6fa72f8c83436a4e5d66d3f2b0b225ce9
gracethomp <gracetomlinson26@gmail.com>
2023-11-21 23:24:51 +0200

    different type of q/a

--- src/ui/common/form/question/Question.js
+++ src/ui/common/form/question/Question.js
@@ -3,7 +3,7 @@ import { Dropdown } from "../../dropdown/Dropdown";
 export const Question = ({ question, answers, onChange }) => {
   return (
     <div className="flex flex-col md:items-center justify-between w-full py-6">
-      <p className="text-2xl">{question}</p>
+      <p className="text-2xl">{question}</p> 
       <Dropdown answers={answers} onChange={onChange} name={question}/>
     </div>
   );
